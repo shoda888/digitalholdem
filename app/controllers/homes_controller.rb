e1c9713670f77b65ba1cdb26958377d2a4468244
GEKKA888 <gekka888@gmail.com>
2018-04-23 01:18:42 +0900

    自分の手札に二枚のトランプがランダムで選ばれるところまで

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,15 +2,13 @@ class HomesController < ApplicationController
   include CreateDeck
 
   def index
-    @player = Player.new
+    @player = Player.create
     @cards = shuffle(build)
 
     @player_cards = drawCard(2)
 
-    # @player.cards.create(suit:@player_cards[0].suit, number:@player_cards[0].number)
-    # @player.cards.create(suit:@player_cards[1].suit, number:@player_cards[1].number)
+    @player.cards.create(suit:@player_cards[0].suit, number:@player_cards[0].number)
+    @player.cards.create(suit:@player_cards[1].suit, number:@player_cards[1].number)
 
-    
-    pp @player_cards[0].suit
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,16 +1,4 @@
 class Player < ApplicationRecord
   has_many :cards, :as => :cardable
 
-  def initialize
-    @hands = []
-  end
-  def draw(deck)
-    @hands.push(deck.drawCard)
-  end
-  def showHands
-    puts "あなたの手札"
-    @hands.each do |hand|
-      hand.show
-    end
-  end
 end
