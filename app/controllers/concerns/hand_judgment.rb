13cedf898d005e481ea02a8fbf706e3b6975c7b1
GEKKA888 <gekka888@gmail.com>
2018-04-28 21:31:31 +0900

    役記述

--- app/controllers/concerns/hand_judgment.rb
+++ app/controllers/concerns/hand_judgment.rb
@@ -2,10 +2,27 @@ module HandJudgement
   extend ActiveSupport::Concern
 
   def hand_judge(targetcards)
-    if isFlash(targetcards)
+    if isRoyal_straight_flush(targetcards)
+      return 'RoyalStraightFlush'
+    elsif isStraight_flush(targetcards)
+      return 'StraightFlash'
+    elsif isFour_cards(targetcards)
+      return 'FourCards'
+    elsif isFull_house(targetcards)
+      return 'FullHouse'
+    elsif isFlush(targetcards)
       return 'Flash'
-    elsif
-
+    elsif isStraight(targetcards)
+      return 'Straight'
+    elsif isThree_cards(targetcards)
+      return 'ThreeCards'
+    elsif isTwo_pairs(targetcards)
+      return 'TwoPairs'
+    elsif isOne_pair(targetcards)
+      return 'OnePair'
+    else isHigh_hand(targetcards)
+      return 'Highhand'
+    end
   end
 
   private
