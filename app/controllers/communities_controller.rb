81b102af81b0488ae1883067da19d8f5de728793
GEKKA888 <gekka888@gmail.com>
2018-07-06 13:59:06 +0900

    adminのカード消し、カードを指定して配布(実験用に移行)

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -16,11 +16,25 @@ class CommunitiesController < ApplicationController
       @community = @game.communities.create#コミュニティー作成
       @cards = shuffle(build) #シャッフルされた52枚のカード
 
-      community_cards = drawCard(5) #コミュニティカード作成
+      community_cards = drawCard([['s',1],['c',7],['s',3],['c',9],['s',5]]) #コミュニティカード作成 #指定
+      # community_cards = drawCard(5) #コミュニティカード作成 #ランダム
       community_cards.each do |c|
         @community.cards.create(suit:c.suit, number:c.number)
       end
+      #被験者のホールカード決定
+      @normal = @game.players.find_by(role: 'normal')
+      @hole = @normal.holes.create(community_id: @community.id)
+      hole_cards = drawCard([['s',4],['s',2]]) #ホールカード作成 #ランダム
+      hole_cards.each do |c|
+        @hole.cards.create(suit:c.suit, number:c.number)
+      end
+      targetcards = @community.cards + @hole.cards
+      @hole.hand = hand_judge(targetcards)
+      @hole.save
+
+      #サクラのホールカード決定
       @game.players.each do |player|
+        next if !player.dealer?
         @hole = player.holes.create(community_id: @community.id)
         hole_cards = drawCard(2) #ホールカード作成
         hole_cards.each do |c|
@@ -41,14 +55,14 @@ class CommunitiesController < ApplicationController
     @community = Community.find(params[:id])
     @player_holes = []
     @community.holes.each do |hole|
-      if !hole.player.normal?
-        @admin_hole = hole
-        @admin = @admin_hole.player
-        @admin_cards = @admin_hole.cards
-        @admin_name = @admin.name
-      else
-        @player_holes << hole
-      end
+      # if !hole.player.normal?
+      #   @admin_hole = hole
+      #   @admin = @admin_hole.player
+      #   @admin_cards = @admin_hole.cards
+      #   @admin_name = @admin.name
+      # else
+      @player_holes << hole
+      # end
     end
   end
   def update
