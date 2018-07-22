e37f584fff9a78a9334d6729a58454f9d306c1b5
GEKKA888 <gekka888@gmail.com>
2018-07-22 22:56:20 +0900

    ルーティングとファビコンの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-
+  root to: "sessions#new"
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :games
